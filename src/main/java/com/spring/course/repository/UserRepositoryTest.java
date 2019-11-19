8359dab9f6d2dc1094f533dab83729507d5af1dc
Rodolfo <santoseuro2018@gmail.com>
2019-11-19 08:23:44 -0300

    initial version

--- src/main/java/com/spring/course/repository/UserRepositoryTest.java
+++ src/main/java/com/spring/course/repository/UserRepositoryTest.java
@@ -1,6 +1,14 @@
 package com.spring.course.repository;
 
- 
-public interface UserRepositoryTest {
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserRepositoryTest {
+
+	
+	
+	
 }
